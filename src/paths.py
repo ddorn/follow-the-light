fd4abd8ba454c323d83ab05a9fa9c4d85533d933
ddorn <diego.dorn@free.fr>
2020-04-08 12:06:19 +0200

    move shaders to src/render/shaders

--- src/paths.py
+++ src/paths.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 TOP_LEVEL_DIR = Path(__file__).parent.parent
-SHADERS_DIR = TOP_LEVEL_DIR / "shaders"
+SHADERS_DIR = TOP_LEVEL_DIR / "src" / "render" / "shaders"
 ASSETS_DIR = TOP_LEVEL_DIR / "assets"
 
 print("Top level:", TOP_LEVEL_DIR)
